e9c8255b4e8d72cc20e47a6427fab4b427e72842
Maxwell Hadley <mrh.github@susato.co.uk>
2021-02-03 10:29:45 +0000

    Add dummy AsyncConfig and AsyncData classes, with tests, to silence warnings from NodeJS Update DeviceCommands.md

---
+++ test/asyncConfig.spec.js
@@ -0,0 +1,35 @@
+const rfxcom = require('../lib'),
+    util = require('util'),
+    matchers = require('./matchers'),
+    FakeSerialPort = require('./helper');
+
+describe('AsyncConfig class', function () {
+    let asyncConfig,
+        fakeSerialPort,
+        device;
+    beforeEach(function () {
+        this.addMatchers({
+            toHaveSent: matchers.toHaveSent
+        });
+        fakeSerialPort = new FakeSerialPort();
+        device = new rfxcom.RfxCom('/dev/ttyUSB0', {
+            port: fakeSerialPort
+        });
+        device.connected = true;
+    });
+    afterEach(function () {
+        device.acknowledge.forEach(acknowledge => {
+            if (typeof acknowledge === "function") {
+                acknowledge()
+            }
+        });
+    });
+
+    describe('instantiation', function () {
+        it('should throw an error if no subtype is specified', function () {
+            expect(function () {
+                asyncConfig = new rfxcom.AsyncConfig(device);
+            }).toThrow("Must provide a subtype.");
+        });
+    });
+});
